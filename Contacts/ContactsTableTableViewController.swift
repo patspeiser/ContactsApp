3d7fa26f3529d20c8afc22501ddfe51d461a1328
pat <pat@MacBook-Pro.home>
2015-02-08 20:37:33 -0500

    add new contact. bug in here. can't edit / add phone number after you create new contact.

--- Contacts/ContactsTableTableViewController.swift
+++ Contacts/ContactsTableTableViewController.swift
@@ -25,6 +25,10 @@ class ContactsTableTableViewController: UITableViewController {
         
         let moveButton = UIBarButtonItem(barButtonSystemItem: .Edit, target: self, action: Selector("toggleEdit"))
         self.navigationItem.leftBarButtonItem = moveButton
+        
+        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: Selector("addContact"))
+        self.navigationItem.rightBarButtonItem = addButton
+        
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -35,6 +39,18 @@ class ContactsTableTableViewController: UITableViewController {
     
     func toggleEdit() {
         self.tableView.setEditing(!self.tableView.editing, animated: true)
+    }
+    
+    func addContact() {
+        let newContact = Contact(name: "New Contact")
+        self.contacts.append(newContact)
+        let newIndexPath = NSIndexPath(forRow: self.contacts.count - 1, inSection: 0)
+        self.tableView.insertRowsAtIndexPaths([newIndexPath], withRowAnimation: .Automatic)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
